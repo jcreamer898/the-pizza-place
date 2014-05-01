6d93f32a10dc91be1e50d0e0b4fb69cff7cd2c3b
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-05-01 16:00:11 -0500

    Stop building compass when running grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -435,7 +435,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', [
         'clean:server',
-        'concurrent:test',
         'autoprefixer',
         'connect:test',
         'karma'
