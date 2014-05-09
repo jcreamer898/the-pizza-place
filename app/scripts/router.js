2a561ff29d2113801ac156d834c0acfaf2cff8c3
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-05-08 22:16:01 -0500

    Change router

---
+++ app/scripts/router.js
@@ -0,0 +1,57 @@
+(function() {
+    var routes = [{
+        href: '/',
+        text: 'Home',
+        title: 'Home',
+        active: true,
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        order: 0
+    }, {
+        href: '/menu',
+        text: 'Menu',
+        title: 'Our Menu',
+        templateUrl: 'views/menu.html',
+        controller: 'MenuCtrl',
+        controllerAs: 'menu',
+        order: 1
+    }, {
+        href: '/contact',
+        text: 'Contact',
+        title: 'Contact Us',
+        templateUrl: 'views/contact.html',
+        order: 2
+    }];
+
+    angular.module('introToAngularApp')
+        .config(function ($routeProvider) {
+            routes.forEach(function(r) {
+                var route = {
+                    title: r.title,
+                    templateUrl: r.templateUrl,
+                    controller: r.controller
+                };
+
+                if (r.controllerAs) {
+                    route.controllerAs = r.controllerAs;
+                }
+
+                $routeProvider.when(r.href, route);
+            });
+
+            $routeProvider
+                .otherwise({
+                    redirectTo: '/'
+                });
+        })
+        .run(function($rootScope) {
+            $rootScope.page = {
+                title: ''
+            };
+
+            $rootScope.$on('$routeChangeSuccess', function (event, current) {
+                $rootScope.page.title = current.title;
+            });
+        })
+        .value('routes', routes);
+}());
